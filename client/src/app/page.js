65d5d278ad4d14d1e35380af13a6c3c5f0e88933
Vivek Sahu <viveksahu1762@gmail.com>
2024-08-09 02:43:01 +0530

    added notlogin page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,18 +6,51 @@ import { usePrivy, useWallets } from "@privy-io/react-auth";
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { AnimatePresence, motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Page = () => {
   const musicPlayer = useSelector((state) => state.musicPlayer);
   const [selectedLayout, setSelectedLayout] = useState("home");
   const [selectedMode, setSelectedMode] = useState("songs");
-  const { ready } = usePrivy();
+  const { ready, authenticated, login } = usePrivy();
   const { wallets } = useWallets();
   const w0 = wallets[0];
   if (!ready)
     return (
       <div className="w-full grid items-center justify-center min-h-screen">
         <Loader />
+      </div>
+    );
+
+  if (!authenticated)
+    return (
+      <div className="relative flex min-h-screen flex-col items-center justify-center p-3.5">
+        <main className="flex flex-col items-center gap-y-9">
+          <div className="max-w-lg space-y-3.5 text-center">
+            <h1 className="text-5xl font-semibold tracking-tight md:text-7xl">
+              Ownsound
+            </h1>
+            <p className="md:text-balance text-muted-foreground md:text-xl">
+              Prototype to demonstrate confidential ERC20 transfers using Inco
+              Network.
+            </p>
+          </div>
+          <div className="flex items-center gap-3.5">
+            <Button onClick={login}>Connect Wallet</Button>
+          </div>
+        </main>
+
+        <footer className="absolute bottom-3.5 mx-auto flex items-center gap-[0.5ch] text-center text-muted-foreground">
+          <span>Powered by</span>
+          <Link
+            href="https://qoneqt.com/"
+            target="_blank"
+            className="group flex items-center text-primary font-semibold gap-[0.5ch] underline-offset-4 hover:underline"
+          >
+            Qoneqt.
+          </Link>
+        </footer>
       </div>
     );
   return (
